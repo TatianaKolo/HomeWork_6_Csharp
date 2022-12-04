d65716c1e1b862faa0d1ec766c2f7327500c413d
TatianaKolo <manila-nick@yandex.ru>
2022-12-04 04:15:58 +0300

    выполнила задачу 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,14 +3,31 @@
 1, -7, 567, 89, 223-> 3
 */
 
-
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int CountPositiv(int[] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
 Console.Clear();
-Console.WriteLine("Введите три числа через пробел: ");
+Console.WriteLine("Введите несколько чисел через пробел: ");
 string input = Console.ReadLine()!;
 int[] Array = GetArrayFromString(input);
 int countNum = CountPositiv(Array);
-int[] GetArrayFromString(string stringArray)
-{
-    
-}
-Console.WriteLine($"Сумма чётных чисел в массиве = {sum}");
+Console.WriteLine($"Количество положительных чисел равно: {countNum}");
